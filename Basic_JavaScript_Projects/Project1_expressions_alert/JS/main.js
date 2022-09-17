37fdf30645c8ab4483e2849cc874a6ae26d7d6e5
3nDr0n <magyarali@gmail.com>
2022-09-17 09:53:42 +0100

    updated code with assignment requirements

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,7 @@
-var xbox="Halo", playstation="Uncharted", nintendo="Zelda"
-document.write(xbox)
+var xbox="Halo", playstation="Uncharted", nintendo="Zelda" //defining multiple variables and giving them sting values
+var xbox=xbox.fontcolor("green") //using the fontcolor method on variables
+var playstation=playstation.fontcolor("blue")
+var nintendo=nintendo.fontcolor("red")
+document.write(xbox) //displaying value of variable
+document.write(2+2)  //displaying result of expression
+window.alert("Do you like videogames?" ) //displays an alert box
