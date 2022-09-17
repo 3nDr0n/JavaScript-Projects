1431a61d69a23c1632d545b360fafcadc1d82bd5
3nDr0n <magyarali@gmail.com>
2022-09-17 10:37:13 +0100

    added Project2_functions folder

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function Total_Monthly_Income() {
+var investments=1400;
+investments +=600;
+var salary=500;
+var sum=investments+salary;
+document.getElementById("Total Income").innerHTML=sum;
+}
+
+
