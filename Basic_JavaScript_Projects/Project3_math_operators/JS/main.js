f67010a8c6c5a055f8b0891e61baec012b131286
3nDr0n <magyarali@gmail.com>
2022-09-19 17:13:25 +0100

    changed operator to subtraction

--- Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
+++ Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -1,6 +1,6 @@
 function number_of_true_love() {
-    var male=45;
-    var female =24;
-    var sum=male+female;
+    var male=87;
+    var female =18;
+    var sum=male-female;
     document.getElementById("Math").innerHTML=sum;
 }
