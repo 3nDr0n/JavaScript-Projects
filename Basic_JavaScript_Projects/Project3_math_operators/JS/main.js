e700cf15a042659b12ec0b35c27b291affb46d71
3nDr0n <magyarali@gmail.com>
2022-09-19 17:29:35 +0100

    changed operators as needed

--- Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
+++ Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -1,6 +1,12 @@
 function number_of_true_love() {
     var male=87;
     var female =18;
-    var sum=male-female;
-    document.getElementById("Math").innerHTML=sum;
+    var sum1=male*female;
+    document.getElementById("Math").innerHTML=sum1;
+}
+function your_lucky_number() {   
+    var partner=48;
+    var lover=12;
+    var sum2=partner / lover;
+    document.getElementById("Math2").innerHTML=sum2;
 }
