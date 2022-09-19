acd5aaca31296468cea4e8fb3a824badb4199fd7
3nDr0n <magyarali@gmail.com>
2022-09-19 18:50:00 +0100

    added required scripts

--- Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
+++ Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -1,12 +1,38 @@
 function number_of_true_love() {
-    var male=87;
-    var female =18;
-    var sum1=male*female;
+    var male=48;
+    var female =21;
+    var sum1=male+female;
     document.getElementById("Math").innerHTML=sum1;
 }
 function your_lucky_number() {   
-    var partner=48;
-    var lover=12;
-    var sum2=partner / lover;
+    var destiny=48;
+    var fate=12;
+    var sum2=destiny/fate;
     document.getElementById("Math2").innerHTML=sum2;
+}
+
+function your_predicted_iq() {
+    var iq=(17+8)*15/2-45;
+    document.getElementById("Math3").innerHTML=iq;
+}
+
+function boring_program() {
+    var bored=256%20;
+    document.getElementById("Math4").innerHTML=bored;
+}
+
+function click_here() {
+    var x=10;
+    document.getElementById("Math5").innerHTML="The effort that went into writing this program is:" + -x;
+}
+    var x=6;
+    x++;
+    var y=6;
+    y--;
+    document.write(x+y+"is the result of a simple math operation using incremented and decremented numbers");
+
+window.alert(Math.random()*10);
+
+function baking() {
+document.getElementById("pi").innerHTML = Math.PI;
 }
