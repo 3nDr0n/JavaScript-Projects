c465d72e789936e7a3562acc9db7085aa3152ec4
3nDr0n <magyarali@gmail.com>
2022-10-05 17:55:26 +0100

    updated code as per requirements

--- Basic_JavaScript_Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic_JavaScript_Projects/Project9_countdown_slideshow/JS/main.js
@@ -12,3 +12,17 @@ function showSlides() { //creates function that show slides on loop
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 3000); // Changes image every 3 seconds
 }
+
+function countdown() {  //countdown function that counts down based on user input
+  var seconds=document.getElementById("seconds").value;
+
+  function tick() {  //displays ticking countdown
+    seconds = seconds -1;
+    timer.innerHTML=seconds;
+    setTimeout(tick, 1000);
+  if(seconds ==-1) {  //conditional statement displays message when countdiwn reached 0 
+    alert("Thats enough now, you'll get hungry!");
+  }  
+      }
+      tick();
+}
